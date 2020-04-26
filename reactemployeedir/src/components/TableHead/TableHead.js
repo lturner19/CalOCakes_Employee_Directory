67bf9788688b3dd81d982f5af0cd8844df808ae4
Leandra Turner <calliet0715@gmail.com>
2020-04-26 14:14:23 -0500

    Creating folder and TableHead.css

---
+++ reactemployeedir/src/components/TableHead/TableHead.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './TableHead.css';
+
+function TableHead(props) {
+    return (
+            <thead>
+                <tr className="head">
+                    <th className="border" scope="col">IMAGE</th>
+                    {/* using this setup with () to allow "first" and "last" to used as an arguments  */}
+                    {/* putting onClick as the th att. allows user to click the heading and sort the names */}
+                    <th className="border" scope="col" onClick={ () =>  props.ascending("first")}> FIRST NAME</th>
+                    <th className="border" scope="col" onClick={ () =>  props.ascending("last")}>LAST NAME</th>
+                    <th className="border"scope="col">PHONE NUMBER</th>
+                    <th className="border"scope="col">EMAIL</th>
+                </tr>
+            </thead>
+    )
+}
+
+export default TableHead;
